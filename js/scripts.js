0dc9e6c4404e937603b948cee819d7cb29f06f10
Łukasz Baraniak <baraniak.l@gmail.com>
2016-11-27 15:29:31 +0100

    pole trójkąta

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 // plik scripts.js
 
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = 0,
+	h = 0,
+	triagleArea = 0;
+
+
+a = prompt('Podaj podstawię trójkąta:');
+h = prompt('Podaj wysokość trójkąta');
+
+triagleArea = a * h / 2;
+
+alert('Pole trójkąta o podstawie a = ' + a + ' i wysokości h = ' + h + ' wynosi: ' + triagleArea);
+console.log('Pole trójkąta o podstawie a = ' + a + ' i wysokości h = ' + h + ' wynosi: ' + triagleArea);
